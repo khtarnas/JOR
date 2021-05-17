3c122aa053a2173c8480ac456250a74601a8c397
Hokua <khtarnas@sas.upenn.edu>
2021-05-17 11:11:53 -0400

    comments clarified

--- experiment_creation/exp_design.py
+++ experiment_creation/exp_design.py
@@ -1,12 +1,19 @@
 '''
 Goal:
-    Get two lists. The first list will be the
+    Get a number of lists. The first list will be the
     order in which words (index positions) are 
     studied (this variable is called "study_order").
     The second list will be the order in which words
     (index positions) are tested (this variable is
-    called "test_order"). These can be used to order
-    the study and test periods in the jsPsych experiment.
+    called "test_order"). The third will be a list indicating
+    the correct side on a test card in the order of test
+    cards. The fourth will be the type of comparison made
+    (e.g. 4 vs. 16) on a test card in the order of test cards.
+    The fifth will be the order of comparisons ordered in the
+    way they were presented (e.g. 68 vs 2 OR 2 vs 68 depending
+    on which item was on the left and right of the card).
+    These can be used to order the study and test periods in
+    the jsPsych experiment.
 '''
 import random
 
@@ -17,18 +24,24 @@ import random
 word_pool = range(261)
 
 '''
-List of studied items:
-    It will start with 25 items that haven't
-    been studied, but are part of the "Many"
-    list. There will be an associated that
-    tracks when the word was studied -- index
-    position will be common for the item and
-    its position. The "Many" list's associated
-    position will be 0.
+To aid in the goal, we will maintain a list of studied items:
+    It will start with 25 items that haven't been studied,
+    but are part of the "Many" list (these are words that
+    will never actually be studied, but will be tested on
+    -- they are called "many" because subjects are asked to
+    treat them as if they occurred "many" items ago). There
+    will be an associated list that tracks when the word was
+    studied -- index positions in the two lists will be common
+    for the item and its position. The "Many" list's associated
+    position will be 0. The associated list will be called
+    "card_nums".
 '''
 # An array of words that can be tested (i.e. are
 # testable). This will start with 25 items ("Many" words).
 testable = random.sample(word_pool, 25)
+
+# A list of words remaining in the wordpool after 25 were removed
+remaining = [x for x in word_pool if x not in testable]
 
 # An array of card numbers. Each number represents the card
 # a word was presented on and the index of that number will
@@ -38,14 +51,14 @@ testable = random.sample(word_pool, 25)
 # appear on any card or they appeared on the 0th card)
 card_nums = [0] * 25
 
-# A list of words remaining in the wordpool after 25 were removed
-remaining = [x for x in word_pool if x not in testable]
-
-# we need 10 'many' words that will not be studied for the prep period
+# we need 10 'many' words that will be studied for the prep period
 # (in order to ensure that the 'many' words are appropriately distributed).
-# There will be 5 'many' words for every 15 cards during the test period so
-# 1/3 of cards in the prep period should have one 'many' word. With 32 cards
-# in the prep period, this means we will have 10 'many' words appearing once every three cards.
+# There will be 5 'many' words for every 15 cards during the test period (of
+# which there are 5 blocks) hence there are 25 many cards in the list of
+# "studied" items. Also because of this, 1/3 of cards in the prep period
+# should have one 'many' word (because 5/15 of the cards in the test period
+# will). With 32 cards in the prep period, this means we will have 10 'many'
+# words appearing once every three cards.
 prep_manys = random.sample(remaining, 10)
 
 # An array of words that need to be studied.
@@ -62,13 +75,14 @@ test_order = []
 # experiment. This is one of our desired outputs.
 study_order = []
 
-# TODO description: the order of test cards and which side is correct on them
+# The order of test cards and which side is correct on them.
 correct_side_order = []
 
-# TODO description: the order of comparison types on the test cards
+# The order of comparison types on the test cards in order of test cards.
 comparison_type_order = []
 
-#TODO description: the order of comparisons with which one went first (on the left)
+# The order of comparisons where the comparison is ordered with the one
+# presented on the left being first rather than in order of recency.
 ordered_comparison_order = []
 
 # A counter to track which card we are currently on. We will start
@@ -76,7 +90,7 @@ ordered_comparison_order = []
 # considered in card distance.
 counter = 1
 
-#TODO: description
+# All of the possible comparisons that can and need to be made in each test block
 comparisons = [
     (0, 2),
     (0, 4),
@@ -96,7 +110,7 @@ comparisons = [
 ] 
 
 '''
-Methods for experiment periods:
+Functions for experiment periods:
 '''
 
 # A helper function used for each study card. It will do the following
@@ -118,19 +132,18 @@ def study(study_order, studiable, testable, card_nums, index):
 # A helper function to determine whether a word is usable for test in the
 # prep period. We DO want to test "many" words, but that functionality is dealt
 # with using a delay and the list called 'prep_manys'. This is the situation in
-# which we are currently NOT doing a many word SO: if a many word is selection we
+# which we are currently NOT doing a many word SO: if a many word is selected we
 # will return False. Also, if there is only one item on the card that we are currently
-# looking at then we'll return False. O.W. we'll return true.
+# looking at then we'll return False . O.W. we'll return true.
 def word_usable(card_nums, index, prev):
-    #print(card_nums)
-    #print(index == 0)
-    #print(card_nums.count(index) == 1)
 
     # make sure a test card does not have two items from the same study card
     if (index == prev):
         return False
     
-    # If even index, return true
+    # If even index, return true (this is because only odd indices can be used
+    # for the test period, because they pull from words that are an even number
+    # back from their index which is odd)
     if (index == 0):
         return False
     else:
@@ -180,7 +193,6 @@ for i in range(counter, 32 + counter):
                 # Get a random item from the testable list and its position in the list
                 item = random.choice(testable)
                 pos = testable.index(item)
-                #print('pos = ' + str(pos))
 
                 # Get the associated card number for the randomly choosen testable item
                 test = card_nums[pos]
@@ -200,12 +212,11 @@ for i in range(counter, 32 + counter):
 # TEST PERIOD (5 sets of 15 sets of scored alternation 
 # between study and test -- starting with study):
 for i in range(5): #5 different test periods
-    #print()
-    #print('entered BLOCK ' + str(i))
 
     # Variables for the block
     failed = False # Boolean indicating whether a block failed
     success = False # Boolean indicating whether a block succeeded
+
     # Create temporary variables for a reset
     count_tracker = 0
     temp_study_order = []
@@ -281,12 +292,6 @@ for i in range(5): #5 different test periods
 
         # Test Period (1 of 5 at a time) (scored alternation between study and test)
         for j in range(15):
-            
-            #print()
-            #print('in BLOCK ' + str(i))
-            #print(temp_card_nums)
-            #print('entered COMPARISON ' + str(j))
-            #print(temp_comparisons)
 
             if failed:
                 break
@@ -295,7 +300,6 @@ for i in range(5): #5 different test periods
             study(temp_study_order, temp_studiable, temp_testable, temp_card_nums, counter)
 
             # Test:
-            #print('Block Options: ' + str(temp))
             tries = []
             tries.extend(temp_comparisons) # make a temp variable for the comparisons again specifically for this while true loop so we don't unnecessarily try a comparison that does not work twice.
 
